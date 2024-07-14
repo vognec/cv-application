de74c4241d423f1067dbacca1ccdc11c176e0765
vognec <137229162+vognec@users.noreply.github.com>
2024-07-14 16:26:02 -0500

    added functionality to display all jobs on cv

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -7,7 +7,18 @@ function Cv({basicInfo, jobInfo}) {
           <p>Last Name: {basicInfo.lastName}</p>
           <p>Email: {basicInfo.email}</p>
           <p>Phone: {basicInfo.phone}</p>
-        
+          <hr />
+          {jobInfo.map( item => (
+            <div key={item.id}>
+                <p>Job Name: {item.jobName}</p>
+                <p>Job Title: {item.jobTitle}</p>
+                <p>Job Duties: {item.jobDuties}</p>
+                <p>Job Start Date: {item.jobStartDate}</p>
+                <p>Job End Date: {item.jobEndDate}</p>
+            </div>
+            )
+          )}
+          
         </div>
 
   );
