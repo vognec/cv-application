0a0242e1ddce50fcbc096e237487bab5cc30685b
vognec <137229162+vognec@users.noreply.github.com>
2024-07-15 21:11:10 -0500

    added formatting for cv employment display

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -13,23 +13,30 @@ function Cv({basicInfo, jobInfo, eduInfo}) {
           </div>
             
             <div className="cv-right-container">
+              <h2>Experience</h2>
+              <hr />
               {jobInfo.map( item => (
                 <div key={item.id}>
-                    <p>{item.jobName}</p>
-                    <p>{item.jobTitle}</p>
-                    <p>{item.jobDuties}</p>
-                    <p>{item.jobStartDate}</p>
-                    <p>{item.jobEndDate}</p>
+                    <div className="display-wrapper">
+                      <p className="item-name">{item.jobName}</p>
+                      <p className="item-title">{item.jobTitle}</p>
+                      <p>{item.jobDuties}</p>
+                      <p className="item-date">{item.jobStartDate} - {item.jobEndDate}</p>
+                    </div>
                 </div>
                 )
               )}
+              <h2>Education</h2>
+              <hr />
               {eduInfo.map( item =>(
-                <div key={item.id}>
-                    <p>{item.eduName}</p>
-                    <p>{item.eduDegree}</p>
-                    <p>{item.eduGPA}</p>
-                    <p>{item.eduStartDate}</p>
-                    <p>{item.eduEndDate}</p>
+                <div className="display-wrapper">
+                  <div key={item.id}>
+                      <p className="item-name">{item.eduName}</p>
+                      <p className="item-title">{item.eduDegree}</p>
+                      <p>{item.eduGPA}</p>
+                      <p className="item-date">{item.eduStartDate} - {item.eduEndDate}</p>
+                     
+                  </div>
                 </div>
               )
               )}
